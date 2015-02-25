82c9d6ea3267a63f76c4a84b20d948e19eb85b00
rpazyaquian <rpaz@localytics.com>
2015-02-25 16:38:58 -0500

    created game window, added naive implementation of game loop

---
+++ game_window.rb
@@ -0,0 +1,22 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+	def initialize
+		super(640, 480, false)
+		# declare background
+		@background = make_background
+		# declare player
+		@player = make_player
+		# declare duck
+		@duck = make_duck
+	end
+
+	def update
+		@updateables.draw
+	end
+	
+	def draw
+		@drawables.draw
+	end
+	
+end
